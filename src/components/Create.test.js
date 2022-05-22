53a9e65435ad54e638f1abc9dde607a15549a36f
garotc <62607844+garotc@users.noreply.github.com>
2022-05-22 18:16:05 +0200

    Update Create.test.js

--- src/components/Create.test.js
+++ src/components/Create.test.js
@@ -1,9 +1,22 @@
-import { render, screen } from '@testing-library/react';
-import { act } from "react-dom/test-utils";
+import { render, renderHook, screen } from '@testing-library/react';
+import { unmountComponentAtNode } from "react-dom";
 import Create from './Create';
+import { createTask } from './Create';
 
-test('renders learn react link', () => {
-    act(() => {
-        fireEvent.click(testElem[0])
-       })
+let container = null;
+beforeEach(() => {
+  // met en place un élément DOM comme cible de rendu
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+test("rendered create", () =>{
+
+})
+
+afterEach(() => {
+    // nettoie en sortie de test
+    unmountComponentAtNode(container);
+    container.remove();
+    container = null;
   });
