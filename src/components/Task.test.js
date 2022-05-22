a679b295fa21bf215ca233ffe0c9f52a2189ecd3
garotc <62607844+garotc@users.noreply.github.com>
2022-05-22 18:17:25 +0200

    Update Task.test.js

--- src/components/Task.test.js
+++ src/components/Task.test.js
@@ -1,6 +1,22 @@
-import { render, screen } from '@testing-library/react';
+import { render, renderHook, screen } from '@testing-library/react';
+import { unmountComponentAtNode } from "react-dom";
 import Task from './Task';
 
-test('renders learn react link', () => {
-
+let container = null;
+beforeEach(() => {
+  // met en place un élément DOM comme cible de rendu
+  container = document.createElement("div");
+  document.body.appendChild(container);
 });
+
+
+test('retourne task', () => {
+    render(<Task />);
+});
+
+afterEach(() => {
+    // nettoie en sortie de test
+    unmountComponentAtNode(container);
+    container.remove();
+    container = null;
+  });
